55fd8d8fc12523d74d5bf84a02904beb9d9f7c5a
leonel <leonel.f@outlook.com>
2014-07-18 15:25:43 -0300

    Se agrega ssh

--- docker/scripts/first_run.sh
+++ docker/scripts/first_run.sh
@@ -2,12 +2,14 @@ USER=admin
 PASS=${PASS:-$(pwgen -s -1 16)}
 
 pre_start_action() {
-  # Echo out info to later obtain by running `docker logs container_name`
-  echo "MARIADB_USER=$USER"
-  echo "MARIADB_PASS=$PASS"
+  # Mostrar informacion sobre el usuario y la contraseña`
+  echo -e "MARIADB_USER=$USER"
+  echo -e "MARIADB_PASS=$PASS"
 }
 
 post_start_action() {
+  #Snippet sacado de otro repo
+
   DB_MAINT_PASS=$(cat /etc/mysql/debian.cnf | grep -m 1 "password\s*=\s*"| sed 's/^password\s*=\s*//')
   mysql -u root -e \
       "GRANT ALL PRIVILEGES ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '$DB_MAINT_PASS';"
@@ -23,6 +25,40 @@ post_start_action() {
       GRANT ALL PRIVILEGES ON *.* TO '$USER'@'%' WITH GRANT OPTION;
 EOF
 
-  echo "Usuario $USER creado correctamente, DB inicializada correctamente"
+  echo -e "Usuario $USER creado correctamente, DB inicializada correctamente"
+
+  echo -e "\n\nConfigurando ssh\n"
+  set_root_ssh_password
+
+  #ya ejecutamos asi que borramos el control
   rm /firstrun
+}
+
+set_root_ssh_password() {
+  #!/bin/bash
+  PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
+  _word=$( [ ${ROOT_PASS} ] && echo -e "preset" || echo -e "random" )
+  echo -e "=> Configurando un ${_word} password para el usuario root"
+  echo -e "root:$PASS" | chpasswd
+
+  echo -e "=> Listo ! :)\n\n"
+  touch /.root_pw_set
+
+  echo -e "==============================SSH=========================================="
+  echo -e "Ahora se pueden conectar via SSH suando:"
+  echo -e ""
+  echo -e "    ssh -p <port> root@<host>"
+  echo -e "    password del usuario root '$PASS'"
+  echo -e ""
+  echo -e "Si lo desean pueden cambiar el password desde adentro, es recomendable!"
+  echo -e "=========================================================================== \n"
+  
+  echo -e "===============================MariaDB====================================="
+  echo -e "Usuario de maria DB:"
+  echo -e ""
+  echo -e "    MARIADB_USER=$USER"
+  echo -e "    MARIADB_PASS=$PASS"
+  echo -e ""
+  echo -e "============================================================================"
+  echo -e "\n\n Leonel Franchelli 0.1"
 }
